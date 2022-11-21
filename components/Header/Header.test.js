6edf297b56c27458e4fab927ea96f39aa17f4328
Julia Katharina Nee <juliakatharina77@icloud.com>
2022-11-21 16:01:23 +0100

    Test1

--- components/Header/Header.test.js
+++ components/Header/Header.test.js
@@ -2,7 +2,7 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import AppHeader from "./Header.js";
 
-test("render Nike Store from Header", () => {
+test("render Soul Master from Header", () => {
   render(<AppHeader />);
   const headerElem = screen.getByText(/Soul Master/i);
   expect(headerElem).toBeInTheDocument();
